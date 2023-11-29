945d5719f431f5b1a2cf4caa73f52fbcd938d2cd
sarmelano <sarmelano.main@gmail.com>
2023-11-29 00:56:30 -0800

    v22

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,46 +1,61 @@
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 function AddContact({ onAdd }) {
-  const [formValues, setFormValues] = useState({
-    name: '',
-    username: '',
-    phone: ''
-  });
-  const [error, setError] = useState('');
   const navigate = useNavigate();
 
-  const handleChange = (e) => {
-    setFormValues({
-      ...formValues,
-      [e.target.name]: e.target.value
-    });
-  };
-
-  const validateFields = () => {
-    const { name, username, phone } = formValues;
-    if(name === '' || username === '' || phone === '') {
-      setError('Пожалуйста, заполните все поля');
-      return false;
-    }
-    return true;
-  };
-
-  const saveContact = () => {
-    if (!validateFields()) return;
-
-    onAdd(formValues);
-    navigate('/');
-  };
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      username: '',
+      phone: ''
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .required('Пожалуйста, заполните это поле'),
+      username: Yup.string()
+        .required('Пожалуйста, заполните это поле'),
+      phone: Yup.string()
+        .required('Пожалуйста, заполните это поле'),
+    }),
+    onSubmit: values => {
+      onAdd(values);
+      navigate('/');
+    },
+  });
 
   return (
-    <form className='addContact'>
-      <input type="text" name="name" value={formValues.name} onChange={handleChange} placeholder="First Name" autoComplete="name" />
-      <input type="text" name="username" value={formValues.username} onChange={handleChange} placeholder="Last Name" autoComplete="username" />
-      <input type="text" name="phone" value={formValues.phone} onChange={handleChange} placeholder="Phone number" autoComplete="tel" />
-      {error && <div>{error}</div>}
+    <form className='addContact' onSubmit={formik.handleSubmit}>
+      <input 
+        type="text" 
+        name="name" 
+        value={formik.values.name} 
+        onChange={formik.handleChange} 
+        placeholder="First Name" 
+        autoComplete="name" 
+      />
+      {formik.errors.name ? <div>{formik.errors.name}</div> : null}
+      <input 
+        type="text" 
+        name="username" 
+        value={formik.values.username} 
+        onChange={formik.handleChange} 
+        placeholder="Last Name" 
+        autoComplete="username" 
+      />
+      {formik.errors.username ? <div>{formik.errors.username}</div> : null}
+      <input 
+        type="text" 
+        name="phone" 
+        value={formik.values.phone} 
+        onChange={formik.handleChange} 
+        placeholder="Phone number" 
+        autoComplete="tel" 
+      />
+      {formik.errors.phone ? <div>{formik.errors.phone}</div> : null}
       <div className='addBtns'>
-        <button type="button" onClick={saveContact}>Save contact</button>
+        <button type="submit">Save contact</button>
         <button className='addBtnLast' type="button" onClick={() => navigate('/')}>Cancel</button>
       </div>
     </form>
